8a3533df121ebcb4a7b49c479127fea8a1ab6791
l3alisidahmed <sidoabdelali06@gmail.com>
2023-09-02 00:23:54 +0100

    remove client folder

--- clients/errors/custom-error.js
+++
@@ -1,9 +0,0 @@
-
-class CustomError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'CustomError';
-  }
-}
-
-module.exports = CustomError
--- clients/errors/index.js
+++
@@ -1,9 +0,0 @@
-const CustomError = require('./custom-error');
-const BadRequestError = require('./bad-request');
-const NotFoundError = require('./not-found');
-
-module.exports = {
-  CustomError,
-  BadRequestError,
-  NotFoundError
-}
