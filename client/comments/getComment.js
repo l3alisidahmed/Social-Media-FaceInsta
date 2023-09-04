8cfda0e622999f05b9fde531fec9ba424411787e
l3alisidahmed <sidoabdelali06@gmail.com>
2023-09-04 12:44:14 +0100

    add link tag

--- client/comments/getComment.js
+++ client/comments/getComment.js
@@ -1,9 +1,9 @@
 let id;
 setTimeout(() => {
-    const link = document.getElementById('link');
     const postBox = document.querySelectorAll('.post-box .post-image');
     postBox.forEach(element => {
         element.addEventListener('click', () => {
+            const link = document.getElementById('link'); 
             // To navigate to a different URL
             localStorage.setItem('postId', element.parentElement.id);
             link.click();
