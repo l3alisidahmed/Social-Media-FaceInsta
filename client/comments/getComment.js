afd99d5b5f118e487e47d2bd8210b566ced1dde8
l3alisidahmed <sidoabdelali06@gmail.com>
2023-09-04 10:47:51 +0100

    change the url

--- client/comments/getComment.js
+++ client/comments/getComment.js
@@ -7,7 +7,7 @@ setTimeout(() => {
         element.addEventListener('click', () => {
             // To navigate to a different URL
             localStorage.setItem('postId', element.parentElement.id);
-            window.location.href = 'http://127.0.0.1:5500/client/comments/index.html';
+            window.location.href = 'https://main--cosmic-crumble-da4920.netlify.app/client/comments/index.html';
         })
     });
 }, 500);
